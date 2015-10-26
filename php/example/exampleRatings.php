aba263a55727a73cd92cde389e6010ad65379a3e
thereido <github@bagowine.com>
2015-10-26 14:58:28 -0400

    Example ratings page used IMDb instead Jinni now

--- php/example/exampleRatings.php
+++ php/example/exampleRatings.php
@@ -1,31 +1,31 @@
 <?php
-$username = "testratingsync";
+$username = "ur60460017";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>RatingSync Test - jinni ratings</title>
+        <title>RatingSync Test - IMDb ratings</title>
     </head>
     <body>
-        <h1>RatingSync Test - jinni ratings (<?php echo $username; ?>)</h1>
+        <h1>RatingSync Test - IMDb ratings (<?php echo $username; ?>)</h1>
 <?php
-require_once "../Jinni.php";
+require_once "../Imdb.php";
 
-$jinni = new \RatingSync\Jinni($username);
+$site = new \RatingSync\IMDb($username);
 
-$films = $jinni->getRatings(1, 1, false);
+$films = $site->getRatings(1, 1, false);
 echo "<h2>Count: " . count($films) . "</h2>";
 
 echo "<table>";
-echo "  <tr><td>Name</td><td>Your Score</td><td>Rated Date</td><td>Content</td><td>pic</td></tr>";
+echo "  <tr><td>Name</td><td>Your Score</td><td>Content</td><td>pic</td></tr>";
 foreach($films as $film) {
-    $rating = $film->getRating("Jinni");
+    $rating = $film->getRating("IMDb");
     echo "<tr>";
     echo "<td>" . $film->getTitle() . "</td>";
     echo "<td>" . $rating->getYourScore() . "</td>";
-    echo "<td>" . $rating->getYourRatingDate()->format("n/j/Y") . "</td>";
+    //echo "<td>" . $rating->getYourRatingDate()->format("n/j/Y") . "</td>";
     echo "<td>" . $film->getContentType() . "</td>";
     echo "<td><img src='" . $film->getImage() . "' /></td>";
     echo "</tr>";
