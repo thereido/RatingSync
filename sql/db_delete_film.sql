ad827b55df247c07b9b475610e2b556d65310dfb
thereido <github@bagowine.com>
2016-09-23 13:34:59 -0400

    Handy sql when you want to delete a film

---
+++ sql/db_delete_film.sql
@@ -0,0 +1,7 @@
+﻿DELETE FROM credit WHERE film_id=(SELECT id FROM film WHERE title LIKE '%FILM TITLE%');
+DELETE FROM film_genre WHERE film_id=(SELECT id FROM film WHERE title LIKE '%FILM TITLE%');
+DELETE FROM rating_archive WHERE film_id=(SELECT id FROM film WHERE title LIKE '%FILM TITLE%');
+DELETE FROM rating WHERE film_id=(SELECT id FROM film WHERE title LIKE '%FILM TITLE%');
+DELETE FROM film_source WHERE film_id=(SELECT id FROM film WHERE title LIKE '%FILM TITLE%');
+DELETE FROM filmlist WHERE film_id=(SELECT id FROM film WHERE title LIKE '%FILM TITLE%');
+DELETE FROM film WHERE title LIKE '%FILM TITLE%';
