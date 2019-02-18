65ca7d8111702bd483c112db691e5ceb06d8f679
thereido <5272831+thereido@users.noreply.github.com>
2019-02-18 09:28:38 -0500

    Change localhost port to 8080

--- Chrome/constants.js
+++ Chrome/constants.js
@@ -1,4 +1,4 @@
 
-var RS_URL_BASE = "http://localhost:55887";
+var RS_URL_BASE = "http://localhost:8080";
 var RS_URL_API = RS_URL_BASE + "/php/src/ajax/api.php";
 var IMDB_FILM_BASEURL = "http://www.imdb.com/title/";
