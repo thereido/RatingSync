cc2108021c0882eccf987c3a90e10e06525b645e
tobule <tobule@allbitszero.io>
2022-05-04 13:40:41 -0400

    Issue #37: Remove temporary sql files from the master branch. They were used for setting up the db for any existing production systems. All known productions have been set up, so the temporary files can be removed.

--- sql/issue37_rename_obsolete_tables.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE rating_archive DROP FOREIGN KEY rating_archive_ibfk_1;
-ALTER TABLE rating_archive DROP FOREIGN KEY rating_archive_ibfk_2;
-ALTER TABLE rating_archive DROP FOREIGN KEY rating_archive_ibfk_3;
-
-RENAME TABLE IF EXISTS rating_archive TO obsolete_rating_archive;
